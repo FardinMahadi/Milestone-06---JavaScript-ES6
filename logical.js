3f997477ea0c5ce4ea5b5a3162db021cc11dd867
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-29 19:26:32 +0600

    logical

---
+++ logical.js
@@ -0,0 +1,20 @@
+// logical operator (and or) && ||
+
+// const x = 5;
+// console.log(x);
+
+// const money = true;
+
+// and &&
+// left && right
+
+// console.log(money && "varsity jabo");
+
+// const bus = "bus ace";
+// const bus = "";
+// const rickshaw = "rickshaw ace";
+
+// console.log(bus || rickshaw);
+
+const x = 5;
+console.log(x === 5 ? "number is 5" : "not five");
