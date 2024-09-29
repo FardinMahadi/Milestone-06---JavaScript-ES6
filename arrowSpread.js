44b6a8318039e0a2afab6235913a264656ab2ae9
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-29 19:46:41 +0600

    arrow spreading

---
+++ arrowSpread.js
@@ -0,0 +1,19 @@
+// const arr1 = [1, 2, 3];
+
+// const arr2 = [...arr1];
+// console.log(arr1, arr2);
+
+// arr2.push(50);
+// arr2.push(60);
+
+// console.log(arr1, arr2);
+
+const names = ["abdul kasem", "kuddus mia", "mokbul hossain"];
+
+// const name1 = names[0];
+// const name2 = names[1];
+// const name3 = names[2];
+
+const [, , z] = names;
+
+console.log(z);
