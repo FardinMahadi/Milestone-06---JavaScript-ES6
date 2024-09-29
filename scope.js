66f8cb114afeb24e3c69d6cbee820ac401ae9bf0
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-29 19:10:31 +0600

    scope starting

---
+++ scope.js
@@ -0,0 +1,37 @@
+/**
+ * 1. 1. global scope
+ * 2. block scope
+ * 3. functional scope
+ */
+
+// const x = 5;
+// // 1
+// console.log(x);
+
+// // 2
+// if (true) {
+//   console.log(x);
+// }
+
+// // 3
+// function show() {
+//   console.log(x);
+// }
+// show();
+
+// {
+//   let x = 15;
+//   console.log(x);
+// }
+
+// console.log(x);
+
+// const x = 5;
+// console.log(x);
+
+{
+  var x = 10;
+  console.log(x);
+}
+
+console.log(x);
