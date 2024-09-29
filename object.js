3e9d05acf91af3a492233baccdd6042bb102562c
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-29 19:56:54 +0600

    object destructuring

---
+++ object.js
@@ -0,0 +1,29 @@
+const myInfo = {
+  name: "fardin",
+  age: 20,
+  address: "narsingdi",
+  sosurName: "katappa",
+  sosurBari: "B.Baria",
+  contact: {
+    phone: 3298,
+    email: "abc@gmail.com",
+  },
+};
+
+// const obj2 = { ...myInfo };
+// obj2.married = false;
+// obj2.age = 26;
+// console.log(myInfo);
+// console.log(obj2);
+
+// const myName = myInfo.name;
+// const age = myInfo.age;
+
+// const { age: boyos } = myInfo;
+// console.log(boyos);
+
+const { contact } = myInfo;
+console.log(contact);
+
+const { email } = contact;
+console.log(email);
