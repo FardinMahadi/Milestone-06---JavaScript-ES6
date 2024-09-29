aeff0239267fe710de9d321f330942c87e696a11
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-29 19:20:47 +0600

    arrow function

---
+++ arrow.js
@@ -0,0 +1,16 @@
+// function info() {
+//   return "ki chai.......?";
+// }
+
+const info = () => "ki chai.......?";
+
+// function double(x) {
+//   return x * 2;
+// }
+
+const double = (x) => x * 2;
+
+const result = info();
+console.log(result);
+
+console.log(double(3));
