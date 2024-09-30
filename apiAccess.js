ed345fa63997210c609a4002ada478965747f972
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-30 19:03:53 +0600

    done

---
+++ apiAccess.js
@@ -0,0 +1,15 @@
+const petOwner = {
+  name: "Jolonto kholil",
+  contact: {
+    phone: "329483",
+    email: "xyz@gmail.com",
+  },
+  pet: {
+    name: "Lofie",
+    info: {
+      color: "black",
+      weight: "1.2kg",
+    },
+  },
+  food: ["apple", "orange", "papaya"],
+};
